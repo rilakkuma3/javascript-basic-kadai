e9f0ffa60230a5a8f393a277db374408d5d944ca
rilakkuma3 <168553423+rilakkuma3@users.noreply.github.com>
2024-05-21 21:33:11 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,14 @@
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+
+
+
+btn.addEventListener('click',() => {
+  setTimeout(() => {
+    text.textContent = 'ボタンをクリックしました'
+  },2000);
+  
+});
+
+
+
